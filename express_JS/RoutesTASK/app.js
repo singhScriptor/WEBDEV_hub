d44dec29be39e88a2790e7840fbc113e8565fcf5
starlord <dhirajkr1117@gmail.com>
2026-05-11 10:20:13 +0530

    implement /orders and /users GET and POST routes

---
+++ express_JS/RoutesTASK/app.js
@@ -0,0 +1,23 @@
+const express=require('express')
+
+const app = express()
+
+app.get('/orders',(req,res)=>{
+    res.send("Here is the list of all orders.")
+})
+
+app.post('/orders',(req,res)=>{
+    res.send("A new order has been created.")
+})
+
+app.get('/users',(req,res)=>{
+    res.send("Here is the list of all users.")
+})
+
+app.post('/users',(req,res)=>{
+    res.send('A new user has been added.')
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000")
+})
