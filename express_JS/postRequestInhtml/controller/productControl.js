201c6f9eb2639b44e8cff0a7be8d942d571c143c
starlord <dhirajkr1117@gmail.com>
2026-05-14 11:55:50 +0530

    added view folder for html file along with completed product control function

--- express_JS/postRequestInhtml/controller/productControl.js
+++ express_JS/postRequestInhtml/controller/productControl.js
@@ -1,11 +1,13 @@
 const path = require('path')
 
 const getProducts = (req,res)=>{
-
+    res.sendFile(path.join(__dirname,"..","view","product.html"))
 }
 
 const postProduct = (req,res)=>{
-
+    const product = req.body
+    console.log(product)
+    req.json({message:'product added successfully',product})
 }
 
 module.exports={
